4bf9ebe592698ebfa3799f3f97812a15c7ec72c3
Ankur Dahiya <legalos.lotr@gmail.com>
2012-02-09 15:59:49 +0530

    Basic infra setup

---
+++ fb-facepalm.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+//
+// @name          FB-hackathon
+// @include       *.facebook.com/*
+// ==/UserScript==
+
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
+// the guts of this userscript
+function main() {
+	$(".UIActionLinks UIActionLinks_bottom").append('facepalm');
+}
+
+// load jQuery and execute the main function
+addJQuery(main);
